6ed051eaa7cedb6c88b408ed3f5e5bb5fdc50050
Ayush Gaikwad <ayushgaikwad4115@gmail.com>
2026-04-21 14:19:54 +0530

    topology.py

--- topology.py
+++ topology.py
@@ -1,21 +1,29 @@
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.cli import CLI
 
-class MyTopo(Topo):
+class SingleSwitchTopo(Topo):
     def build(self):
-        # Create switch
+        # Create one switch
         s1 = self.addSwitch('s1')
 
-        # Create hosts
+        # Create four hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
         h4 = self.addHost('h4')
 
-        # Create links
+        # Connect all hosts to the switch
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(h3, s1)
         self.addLink(h4, s1)
 
-# Register topology
-topos = {'mytopo': (lambda: MyTopo())}
+if __name__ == '__main__':
+    topo = SingleSwitchTopo()
+    net = Mininet(topo=topo, controller=RemoteController)
+    net.start()
+    print("Network Started")
+    CLI(net)
+    net.stop()
